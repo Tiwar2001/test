8b64b3e7378f0d7dcf0b3fb64792a8f73a9e78c0
Pushpendra Tiwari <132486872+Tiwar2001@users.noreply.github.com>
2025-02-06 12:34:27 +0530

    h1.py
    remove samosa

--- h1.py
+++ h1.py
@@ -2,7 +2,7 @@ hungry=input("are you hungry?")
 if hungry=="yes":
     print("eat fruits")
     print("eat Burger")
-    print("eat samosa")
+   
 else:
     thirsty=input("Are You thirsty?")
     if thirsty=="yes":
